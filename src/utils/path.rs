0a36dd66796b631cf2025a24c13b120c470af46b
ttzztztz <ttzztztz@outlook.com>
2020-09-21 20:21:11 +0800

    feat: path with dev

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -1,11 +1,34 @@
+use std::env;
+
+fn is_dev_mode() -> bool {
+    let mut dev_mode = false;
+    if let Ok(_) = env::var("DEV") {
+        dev_mode = true;
+    }
+
+    dev_mode
+}
+
 pub fn docker_case_path(case_id: u32) -> String {
-    format!("/case/{}.in", case_id)
+    if is_dev_mode() {
+        format!("./case/{}.in", case_id)
+    } else {
+        format!("/case/{}.in", case_id)
+    }
 }
 
 pub fn docker_output_path(case_id: u32) -> String {
-    format!("/output/{}.out", case_id)
+    if is_dev_mode() {
+        format!("./output/{}.out", case_id)
+    } else {
+        format!("/output/{}.out", case_id)
+    }
 }
 
 pub fn docker_result_file() -> String {
-    String::from("/result/info.json")
+    if is_dev_mode() {
+        String::from("./result/info.json")
+    } else {
+        String::from("/result/info.json")
+    }
 }
