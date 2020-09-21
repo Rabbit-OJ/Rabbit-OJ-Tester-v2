9a29e33dcf281647b3cb5a925c7bf60478baafd2
ttzztztz <ttzztztz@outlook.com>
2020-09-21 13:46:36 +0800

    chore

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, thread};
+use std::process;
 use std::process::{Child, Command};
 use nix::unistd::{Pid, setpgid};
 
@@ -31,7 +32,7 @@ fn main() {
     }
 
     let mut test_result: Vec<TestResult<'static>> = vec![];
-    for i in 1..case_count {
+    for i in 1..(case_count + 1) {
         println!("Testing Case #{} ...", i);
         let mut command = Command::new(exec_command_name);
         command.args(exec_args);
@@ -43,6 +44,60 @@ fn main() {
     let test_result_json_str = serde_json::to_string(&test_result).unwrap();
     write_result_file(test_result_json_str).unwrap();
     std::process::exit(0);
+}
+
+fn test_one(mut command: Command, index: u32, time_limit: u64, space_limit: u64) -> TestResult<'static> {
+    let mut result = TestResult {
+        case_id: index,
+        status: consts::STATUS_OK,
+        time_used: 0,
+        space_used: 0,
+    };
+
+    let peak_memory: Arc<RwLock<u64>> = Arc::new(RwLock::new(0));
+    let child_process: Child;
+
+    let stdin_file = open_stdin_file(index).unwrap();
+    let stdout_file = create_stdout_file(index).unwrap();
+    command.stdin(stdin_file);
+    command.stdout(stdout_file);
+
+    match command.spawn() {
+        Ok(cmd) => {
+            child_process = cmd;
+        }
+        Err(e) => {
+            println!("{}", e);
+            result.status = consts::STATUS_RE;
+
+            return result;
+        }
+    }
+
+    let start_time = time::now_unix();
+    let child_pid = child_process.id();
+
+    let current_pid = process::id();
+    if let Err(e) = setpgid(Pid::from_raw(child_pid as i32), Pid::from_raw(current_pid as i32)) {
+        println!("[WARNING] setpgid syscall executed error due to {}, child_pid = {}, current_pid = {}", e, child_pid, current_pid);
+    }
+
+    let (tx, rx) = mpsc::channel::<&'static str>();
+    let ctx = Arc::new(Mutex::new(true));
+
+    main_thread(child_process, tx.clone(), ctx.clone());
+    memory_watch_thread(child_pid as i32, space_limit, tx.clone(), peak_memory.clone(), ctx.clone());
+    time_watch_thread(time_limit, tx.clone(), ctx.clone());
+
+    let msg = rx.recv().unwrap();
+    let end_time = time::now_unix();
+
+    let space_used = peak_memory.read().unwrap();
+    result.space_used = *space_used;
+    result.time_used = (end_time - start_time) as u64;
+    result.status = msg;
+
+    result
 }
 
 fn main_thread(child_process: Child, tx: Sender<&'static str>, ctx: Arc<Mutex<bool>>) {
@@ -103,7 +158,7 @@ fn memory_watch_thread(pid: i32, memory_limit: u64, status_tx: Sender<&'static s
                     Ok(mut guard) => {
                         *guard = current_memory;
                         drop(guard);
-                    },
+                    }
                     _ => {}
                 }
 
@@ -120,56 +175,4 @@ fn memory_watch_thread(pid: i32, memory_limit: u64, status_tx: Sender<&'static s
             }
         }
     });
-}
-
-fn test_one(mut command: Command, index: u32, time_limit: u64, space_limit: u64) -> TestResult<'static> {
-    let mut result = TestResult {
-        case_id: index,
-        status: consts::STATUS_OK,
-        time_used: 0,
-        space_used: 0,
-    };
-
-    let peak_memory: Arc<RwLock<u64>> = Arc::new(RwLock::new(0));
-    let child_process: Child;
-
-    let stdin_file = open_stdin_file(index).unwrap();
-    let stdout_file = create_stdout_file(index).unwrap();
-    command.stdin(stdin_file);
-    command.stdout(stdout_file);
-
-    match command.spawn() {
-        Ok(cmd) => {
-            child_process = cmd;
-        }
-        Err(e) => {
-            println!("{}", e);
-            result.status = consts::STATUS_RE;
-
-            return result;
-        }
-    }
-
-    let start_time = time::now_unix();
-    let child_pid = child_process.id();
-    if let Err(e) = setpgid(Pid::from_raw(child_pid as i32), Pid::from_raw(child_pid as i32)) {
-        println!("[WARNING] setpgid syscall executed error due to {}", e);
-    }
-
-    let (tx, rx) = mpsc::channel::<&'static str>();
-    let ctx = Arc::new(Mutex::new(true));
-
-    main_thread(child_process, tx.clone(), ctx.clone());
-    memory_watch_thread(child_pid as i32, space_limit, tx.clone(), peak_memory.clone(), ctx.clone());
-    time_watch_thread(time_limit, tx.clone(), ctx.clone());
-
-    let msg = rx.recv().unwrap();
-    let end_time = time::now_unix();
-
-    let space_used = peak_memory.read().unwrap();
-    result.space_used = *space_used;
-    result.time_used = (end_time - start_time) as u64;
-    result.status = msg;
-
-    result
 }
